eb137768907582541083e3ba161e5e17f67744e9
Muhammad Haseeb <mh6218@nyu.edu>
2023-09-21 10:31:53 -0400

    moving mvfst into libs and rebuilding

---
+++ cmake_build/CMakeFiles/echo.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/echo.dir/echo/main.cpp.o"
+  "CMakeFiles/echo.dir/echo/main.cpp.o.d"
+  "echo"
+  "echo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/echo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
