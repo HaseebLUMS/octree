ad9c07c24ad5826949544263ee34f938e1cd4340
HaseebLUMS <mh6218@nyu.edu>
2024-02-28 19:37:38 +0000

    added structure for octavius transport

--- src/octavius_transport/client.hpp
+++ src/octavius_transport/client.hpp
@@ -1,13 +1,25 @@
 class client {
-private:
-    /* data */
 public:
-    client(/* args */);
-    ~client();
+    client() {}
+    int attempt_connection();
 };
 
-client::client(/* args */) {
+int client::attempt_connection() {
+    // attempts to connect to the server
+    // upon connection, spins `receive_data` and `process_data` threads
 }
 
-client::~client() {
+void receive_data() {
+    // will keep on receiving data from the server
+    // any received data will be passed to `process_data` via a queue
+}
+
+void process_data() {
+    // will keep on checking for new data in the queue
+    // upon any new data, it will try to construct a frame
+    // when a frame is completed, it will be pushed to `available_frames` queue
+}
+
+void get_frame() {
+    // will check the `available_frames` queue and return the first stored frame
 }
--- src/octavius_transport/server.hpp
+++ src/octavius_transport/server.hpp
@@ -1,13 +1,23 @@
 class server {
-private:
-    /* data */
 public:
-    server(/* args */);
-    ~server();
+    server() {}
+    int send_frame();
+    void listen_for_connections();
+    void serve_client();
 };
 
-server::server(/* args */) {
+int server::send_frame() {
+    // serialize the reliable and unreliable parts
+    // create necessary metadata (size of unreliable parts ?)
+    // enqueue the data in all the client queues
 }
 
-server::~server() {
+void server::listen_for_connections() {
+    // accept new connections
+    // for each new connections, spin a thread `serve_client`, make a new client queue
+}
+
+void server::serve_client() {
+    // keep watching the respective queue for new data
+    // upon new data, send it to the client using quiche
 }
