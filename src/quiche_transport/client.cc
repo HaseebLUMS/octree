1633b15a1bc758c344d6331422d3f0c5f352eb0e
HaseebLUMS <mh6218@nyu.edu>
2024-03-22 12:49:46 +0000

    using reno

--- src/quiche_transport/client.cc
+++ src/quiche_transport/client.cc
@@ -329,7 +329,7 @@ int main(int argc, char *argv[]) {
     quiche_config_verify_peer(config, false);
     quiche_config_enable_pacing(config, true);
 
-    quiche_config_set_cc_algorithm(config, QUICHE_CC_BBR);
+    quiche_config_set_cc_algorithm(config, QUICHE_CC_RENO);
     // quiche_config_enable_hystart(config, true);
 
     if (getenv("SSLKEYLOGFILE")) {
