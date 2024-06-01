a66e328795cdb57c382dec5ed4b1f98abe6f9840
HaseebLUMS <mh6218@nyu.edu>
2024-06-01 18:08:40 -0400

    added psnr,ssim calculations by ignoring transparent background

--- ssim.py
+++ ssim.py
@@ -2,6 +2,7 @@ import numpy as np
 from PIL import Image
 from skimage.metrics import structural_similarity as ssim
 import sys
+import math
 
 def load_image(image_path):
     """Load an image from a file path and return it as a numpy array."""
@@ -16,15 +17,18 @@ def extract_non_transparent_regions(image_array):
 
 def calculate_ssim(image1, image2, win_size=7):
     """Calculate SSIM between two images ignoring transparent background."""
-    mask1 = extract_non_transparent_regions(image1)
-    
-    # Extract non-transparent regions
-    image1_non_transparent = image1[mask1][:, :3]
-    image2_non_transparent = image2[mask1][:, :3]
 
     # Calculate SSIM on non-transparent regions
-    ssim_value = ssim(image1_non_transparent, image2_non_transparent, multichannel=True, win_size=win_size)
+    ssim_value = ssim(image1, image2, multichannel=True, win_size=win_size)
     return ssim_value
+
+def calculate_psnr(image1, image2):
+    """Calculate Peak Signal-to-Noise Ratio (PSNR) between two images."""
+
+    mse = np.mean((image1 - image2) ** 2)
+    max_pixel = 255.0
+    psnr = 20 * math.log10(max_pixel / math.sqrt(mse))
+    return psnr
 
 # Paths to the images
 image1_path = sys.argv[1]
@@ -34,6 +38,15 @@ image2_path = sys.argv[2]
 image1 = load_image(image1_path)
 image2 = load_image(image2_path)
 
+mask = extract_non_transparent_regions(image1)
+
+# Extract non-transparent regions
+image1_non_transparent = image1[mask][:, :3]  # get rid of alpha channel
+image2_non_transparent = image2[mask][:, :3]
+
 # Calculate SSIM
-ssim_value = calculate_ssim(image1, image2, win_size=3)
+ssim_value = calculate_ssim(image1_non_transparent, image2_non_transparent, win_size=3)
 print(f"SSIM: {ssim_value}")
+
+psnr_value = calculate_psnr(image1_non_transparent, image2_non_transparent)
+print(f"PSNR: {psnr_value}")
